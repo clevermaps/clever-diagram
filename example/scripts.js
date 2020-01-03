8b7fa127ba94a91479a13256e3902ce7d7466c9a
Marek Herda <marek.herda@cleveranalytics.com>
2020-01-03 13:38:27 +0100

    Do zoom and pan without translateExtent
    * So that we can zoom to the cursor position
    * Use diagramMargin property instead of css padding on container
    * Remove zoomEnd event and handler
    * Set transform old way (without d3.event.transform function), so that we can use own object with x, y and k

--- example/scripts.js
+++ example/scripts.js
@@ -44,15 +44,14 @@ class Example {
                 console.log('unhighlight node');
                 this.diagram.unhighlightNode();
             })
-            .on('zoomEnd', (scale) => {
-                this.currentScale = scale;
-                console.log(`zoom scale: ${scale}`);
+            .on('zoom', (transform) => {
+                console.log(`zoom transform: ${transform}`);
+                this.diagram.setTransform(transform);
+                this.currentScale = transform.k;
             });
 
         d3.json(data[this.variant].path).then(json => {
-            this.diagram.setData(json).then(() => {
-                this.scaleExtent = this.diagram.getZoomScaleExtent();
-            });
+            this.diagram.setData(json);
         });
 
         this.registerEvents();
@@ -93,13 +92,12 @@ class Example {
         });
 
         d3.select('#fullExtent').on('click', () => {
-            this.diagram.setZoom(this.scaleExtent[0]);
+            this.diagram.setFullExtent();
         });
 
         d3.select('#reloadZoom').on('click', () => {
             this.wrapper.style.width = '700px';
             this.diagram.reloadZoom();
-            this.scaleExtent = this.diagram.getZoomScaleExtent();
         });
     }
 }
